b132c5f17b729751a5adaf1f08d7f251f4f53924
huuloc2026 <huuloc2026@gmail.com>
2024-11-18 00:43:53 +0700

    helloworld

---
+++ src/HELLOWORLD/HelloWorld.java
@@ -0,0 +1,10 @@
+package HELLOWORLD;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World with Jake Bui 2");
+		System.out.println("Hello World with Jake Bui 2");
+		System.out.println("Hello World with Jake Bui 2");
+		System.out.println("Hello World with Jake Bui 2");
+	}
+}
